63778d67f92a80d7441090a45ede63cb11a38609
Uzoma Emeka <uzomaemeka260@gmail.com>
2026-09-23 18:23:45 -0700

    fix health check threshold to prevent instance flapping

--- modules/single_region_infra/main.tf
+++ modules/single_region_infra/main.tf
@@ -226,6 +226,11 @@ resource "aws_iam_role_policy_attachment" "ssm_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "s3_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
 resource "aws_iam_instance_profile" "ssm_profile" {
   name = "EC2-SSM-Profile-VPC"
   role = aws_iam_role.ssm_role.name
@@ -277,6 +282,15 @@ resource "aws_lb_target_group" "nginx" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    interval            = 30
+    timeout             = 10
+    matcher             = "200-399"
+  }
 }
 
 resource "aws_lb_listener" "public" {
